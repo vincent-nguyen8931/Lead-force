fbf5c2904c41fbfd9fe9d47e440ef6090099c4c7
vincent-nguyen8931 <vincent.nguyen8931@gmail.com>
2021-03-05 14:23:40 -0800

    remove app.js

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Member from "./pages/Member"
+// import Main from "./pages/main";
+// import Jumbotron from "./components/jumbotron"
+
+import Nav from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Member />
+      {/* <Jumbotron />
+      <Main /> */}
+    </>
+  );
+}
+
+export default App;
