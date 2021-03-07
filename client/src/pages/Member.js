e19036c5a7735de2ba5c7464d88bfcd14c8f5654
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-03-06 17:20:17 -0800

    Successfully have frontend button click respond to backend database.

--- client/src/pages/Member.js
+++ client/src/pages/Member.js
@@ -1,45 +1,92 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import axios from "axios";
 import API from "../utils/API"
 
 class Member extends Component {
-    state = {
+
+
+/*
+  var [username, setUsername] = useState("");
+  var [password, setPassword] = useState("");
+  var [email, setEmail] = useState("");
+  */
+  constructor(props) {
+    super(props)
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onClick = this.onClick.bind(this);
+  
+    this.state = {
       username: "",
       password: "",
       email: ""
     };
-
-signUp = () => {
-
- API.signUp().then(data => {
-   this.setState({
-     username: data.username,
-     password: data.password,
-     email: data.email
-   })
-   .catch(err => console.log(err));
-   
- })
-}
-
-handleInputChange = event => {
-  var fieldName = event.target.name;
-  var fieldValue = event.target.value;
-  this.setState({
-    [fieldName]: fieldValue
-  }); 
-}
-
-  render() {
-    return(
-      <div>
- <input onChange={this.handleInputChange} name="username" value={this.state.username}/>
- <input onChange={this.handleInputChange} name="password" value={this.state.password}/>
- <input onChange={this.handleInputChange} name="email" value={this.state.email}/>
-
- <button onClick={this.signUp}>Sign Up</button>
- </div>
-    )
   }
+    
+
+  onChangeUsername(e) {
+    this.setState({username: e.target.value})
+  }
+
+  onChangePassword(e) {
+    this.setState({password: e.target.value})
+  }
+
+  onChangeEmail(e) {
+    this.setState({email: e.target.value})
+  }
+
+onClick(e) {
+  e.preventDefault()
+
+var userObject = {
+  username: this.state.username,
+  password: this.state.password,
+  email: this.state.email
+};
+
+  axios.post("/api/registerUser", userObject).then(res => {
+   console.log(res.data)
+    })
+    .catch(err => console.log(err));
+
+    this.setState({username: "", password: "", email: ""})
+    
+  };
+
+
+// handleInputChange = event => {
+//   var fieldName = event.target.name;
+//   var fieldValue = event.target.value;
+//   this.setState({
+//     [fieldName]: fieldValue
+//   }); 
+// }
+
+  // var handleInputChange = e => {
+  //   e.preventDefault();
+  //   console.log("username is " + username);
+  //   console.log("password is " + password);
+  //   console.log("email is " + email);
+  //   API.signUp().then(data => console.log(data))
+  //     .catch(err => console.log(err));
+
+  // }
+
+render() {
+  return (
+    <div>
+      <input onChange={this.onChangeUsername} name="username" value={this.state.username} />
+      <input onChange={this.onChangePassword} name="password" value={this.state.password} />
+      <input onChange={this.onChangeEmail} name="email" value={this.state.email} />
+
+      <button onClick={this.onClick}>Sign Up</button>
+    </div>
+  )
+
+}
 }
 
 export default Member;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   signUp: function () {
-    return axios.post("/api/reigsterUser")
+    return axios.post("/api/registerUser")
   }
 }
