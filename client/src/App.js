486b91452a2b9a82e63a1caf9fe2e0dd730050b7
austinwoo123 <austinwoo123@gmail.com>
2021-03-06 18:26:25 -0800

    create login page and edit styling

--- client/src/App.js
+++ client/src/App.js
@@ -8,20 +8,28 @@ import About from "./components/About";
 import Contact from "./components/Contact"
 import Footer from "./components/Footer";
 import Description from "./components/Description";
+import Login from "./pages/Login";
+import { BrowserRouter as Route, Router } from "react-router-dom";
+
 function App() {
   return (
-    <>
+    // <Router>
+    <div >
+
       <Nav />
-      <Header />
-      <About />
-      <Description />
-      <Contact />
-      <Member />
+      {/* <Header /> */}
+      {/* <About /> */}
+      {/* <Description /> */}
+      {/* <Contact /> */}
+      {/* <Member /> */}
       <Footer />
+      <Login />
+      {/* <Route exact path="/login" component={Login} /> */}
 
       {/* <Jumbotron />
       <Main /> */}
-    </>
+    </div>
+
   );
 }
 
--- client/src/pages/Member.js
+++ client/src/pages/Member.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import style from "./member.css"
+// import style from "./member.css"
 class Member extends Component {
 
   constructor() {
@@ -34,34 +34,34 @@ class Member extends Component {
     });
   }
 
-  render() {
-    return (
-      <div className="row">
-        <div className="col-6" >
-          <div className="div-login">
-            <h2 className="signup">SignUp</h2>
-            <div className="linebreak"></div>
-            <input onChange={this.handleInputChange} placeholder="Username" name="username" value={this.state.username} />
-            <input onChange={this.handleInputChange} placeholder=" Password" name="password" value={this.state.password} />
-            <input onChange={this.handleInputChange} placeholder=" Email Address" name="email" value={this.state.email} />
-            <button onClick={this.signUp}>Sign Up</button>
-          </div >
+  // render() {
+  //   return (
+  //     <div className="row">
+  //       <div className="col-6" >
+  //         <div className="div-login">
+  //           <h2 className="signup">SignUp</h2>
+  //           <div className="linebreak"></div>
+  //           <input onChange={this.handleInputChange} placeholder="Username" name="username" value={this.state.username} />
+  //           <input onChange={this.handleInputChange} placeholder=" Password" name="password" value={this.state.password} />
+  //           <input onChange={this.handleInputChange} placeholder=" Email Address" name="email" value={this.state.email} />
+  //           <button onClick={this.signUp}>Sign Up</button>
+  //         </div >
 
-        </div>
-        <div className="col-6" >
-          <div className="div-login1">
-            <h2 className="signup">Login</h2>
-            <div className="linebreak"></div>
-            <input onChange={this.handleInputChange} placeholder="Username" name="username" value={this.state.username} />
-            <input onChange={this.handleInputChange} placeholder="Password" name="password" value={this.state.password} />
-            <input onChange={this.handleInputChange} placeholder="Email Address" name="email" value={this.state.email} />
-            <button onClick={this.signUp}>Login</button>
-          </div >
-        </div>
-      </div>
+  //       </div>
+  //       <div className="col-6" >
+  //         <div className="div-login1">
+  //           <h2 className="signup">Login</h2>
+  //           <div className="linebreak"></div>
 
-    )
-  }
+  //           <input onChange={this.handleInputChange} placeholder="Email Address" name="email" value={this.state.email} />
+  //           <input onChange={this.handleInputChange} placeholder="Password" name="password" value={this.state.password} />
+  //           <button onClick={this.signUp}>Login</button>
+  //         </div >
+  //       </div>
+  //     </div>
+
+  //   )
+  // }
 }
 
 export default Member;
