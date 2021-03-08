c2de32b831de3e8560ed609c588f47f96ed27181
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-03-07 17:34:28 -0800

    Refactor App.js to perform page routing only

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,18 @@
-import React, {Router, Route} from 'react';
-import Member from "./pages/Member"
-// import Main from "./pages/main";
-// import Jumbotron from "./components/jumbotron"
-import Header from "./components/Header";
-import Nav from "./components/Navbar";
-import About from "./components/About";
-import Contact from "./components/Contact"
-import Footer from "./components/Footer";
-import Description from "./components/Description";
+import React from 'react';
+import Home from "./pages/Home"
+import Login from "./pages/Login";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
+    <BrowserRouter>
     <div>
-      <Member />
-      <Nav />
-      <Header />
-      <About />
-      <Description />
-      <Contact />
-      <Footer />
-      
-    {/* <Route exact path="/"></Route> */}
-
-      {/* <Jumbotron />
-      <Main /> */}
+      <Switch>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/" component={Home} />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
