cf97f88b3fbd21190e8da36415a41ce2484f8313
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-03-08 13:42:37 -0800

    Add code to pull getAllLeads route information and store into table."

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,33 +1,43 @@
-import React from "react";
-import style from "./style.css"
+import React, { Component } from "react";
+import style from "./style.css";
 import TableData from "../TableData";
+import API from "../../utils/API";
 
-export default function Table() {
+export default class Table extends Component {
+
+  getEveryLead() {
+    API.getAllLeads().then(res => {
+      console.log(res.data)
+    })
+      .catch(err => console.log(err));
+  }
+
+  render() {
     return (
-        <div className="table1">
-            <table className="table table-bordered table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Full Name</th>
-                        <th scope="col">Email Address</th>
-                        <th scope="col">Phone Number</th>
-                        <th scope="col">Oppurtunity</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map(item =>
-                        <TableData
-                            name={item.name}
-                            email={item.email}
-                            phone={item.phone}
-                            oppurtunity={item.oppurtunity}
-                        />)}
+      <div className="table1">
+        <table className="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Full Name</th>
+              <th scope="col">Email Address</th>
+              <th scope="col">Phone Number</th>
+              <th scope="col">Oppurtunity</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.getEveryLead.map(item =>
+              <TableData
+                name={item.name}
+                email={item.email}
+                phone={item.phone}
+                oppurtunity={item.oppurtunity}
+              />)}
 
-                </tbody>
-            </table>
-        </div >
+          </tbody>
+        </table>
+      </div >
     )
+  }
 }
-
