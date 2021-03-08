40f43dbdcdf6fa5d8836beac4f252bc61347104f
austinwoo123 <austinwoo123@gmail.com>
2021-03-08 12:58:06 -0800

    update build foler and package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Home from "./pages/Home"
 import Login from "./pages/Login";
+import Members from "./pages/Members";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <Switch>
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/" component={Home} />
-      </Switch>
-    </div>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/members" component={Members} />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
