8cecacc0d14b0e498057ea0a1f8dc90a2221af31
austinwoo123 <austinwoo123@gmail.com>
2021-03-09 13:45:30 -0800

    deleted footer component

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,12 +6,6 @@ export default function About() {
     return (
         <div className="jumbotron jumbotron-fluid" id="about">
             <div className="container">
-<<<<<<< Updated upstream
-                <h1 className="about-Name">Leads Force</h1>
-                <h2 className="Lead-Header">| Lead Management System |</h2>
-=======
-
->>>>>>> Stashed changes
             </div>
         </div>
     )
