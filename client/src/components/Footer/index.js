c4734ea2702085482ae14ed0e129f4414d3979e4
austinwoo123 <austinwoo123@gmail.com>
2021-03-09 11:05:09 -0800

    edit styling of footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,21 +1,117 @@
 import React from "react";
 import style from "./style.css"
+import { Link } from 'react-router-dom';
 // import styled from "styled-components"
 
 
 function Footer() {
     return (
-        <div class="container" id="footer">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light .navbar-expand{-sm|-md|-lg|-xl} ">
-                <a class="navbar-brand"> &copy;{new Date().getFullYear()} Lead Force | All rights reserved |
-             Terms Of Service | Privacy</a>
-            </nav>
+
+        <div className='footer-container'>
+            <section className='footer-subscription'>
+                <p className='footer-subscription-heading'>
+                    Register to start managing your leads!
+          </p>
+            </section>
+            <div class='footer-links'>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Contact Us</h2>
+                        <Link to='/'>Contact</Link>
+                        <Link to='/'>Support</Link>
+                        <Link to='/'>Destinations</Link>
+                        <Link to='/'>Sponsorships</Link>
+                    </div>
+                </div>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>Videos</h2>
+                        <Link to='/'>Submit Video</Link>
+                        <Link to='/'>Ambassadors</Link>
+                        <Link to='/'>Agency</Link>
+                        <Link to='/'>Influencer</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Social Media</h2>
+                        <Link to='/'>Instagram</Link>
+                        <Link to='/'>Facebook</Link>
+                        <Link to='/'>Youtube</Link>
+                        <Link to='/'>Twitter</Link>
+                    </div>
+                </div>
+            </div>
+            <section class='social-media'>
+                <div class='social-media-wrap'>
+                    <small class='website-rights'> &copy;{new Date().getFullYear()} Leads Force | All rights reserved |
+     Terms Of Service | Privacy</small>
+                    <div class='social-icons'>
+                        <Link
+                            class='social-icon-link facebook'
+                            to='/'
+                            target='_blank'
+                            aria-label='Facebook'
+                        >
+                            <i class='fab fa-facebook-f' />
+                        </Link>
+                        <Link
+                            class='social-icon-link instagram'
+                            to='/'
+                            target='_blank'
+                            aria-label='Instagram'
+                        >
+                            <i class='fab fa-instagram' />
+                        </Link>
+                        <Link
+                            class='social-icon-link youtube'
+                            to='/'
+                            target='_blank'
+                            aria-label='Youtube'
+                        >
+                            <i class='fab fa-youtube' />
+                        </Link>
+                        <Link
+                            class='social-icon-link twitter'
+                            to='/'
+                            target='_blank'
+                            aria-label='Twitter'
+                        >
+                            <i class='fab fa-twitter' />
+                        </Link>
+                        <Link
+                            class='social-icon-link twitter'
+                            to='/'
+                            target='_blank'
+                            aria-label='LinkedIn'
+                        >
+                            <i class='fab fa-linkedin' />
+                        </Link>
+                    </div>
+                </div>
+            </section>
         </div>
-        //             &copy;{new Date().getFullYear()} Lead Force | All rights reserved |
-        //     Terms Of Service | Privacy
-        //   </div>
-        // </div>
     );
 }
+
+
+
+
+
+
+
+
+
+// <div class="container" id="footer">
+//     <nav class="navbar navbar-expand-lg navbar-light bg-light .navbar-expand{-sm|-md|-lg|-xl} ">
+//         <a class="navbar-brand"> &copy;{new Date().getFullYear()} Lead Force | All rights reserved |
+//      Terms Of Service | Privacy</a>
+//     </nav>
+// </div>
+//             &copy;{new Date().getFullYear()} Lead Force | All rights reserved |
+//     Terms Of Service | Privacy
+//   </div>
+// </div>
 
 export default Footer;
